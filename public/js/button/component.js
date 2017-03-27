99007d0d84112aa3dd0cb92b36708bae250d1b77
Ky Truong <kytruong@kms-technology.com>
2017-03-27 15:53:26 +0700

    Scroll not being added automatically with autoResize=true

--- public/js/button/component.js
+++ public/js/button/component.js
@@ -6,23 +6,12 @@ window.MyButton = xcomponent.create({
     // The html tag used to render my component
     tag: 'my-button-component',
 
-    contexts: {
-        iframe: true,
-        lightbox: true,
-        popup: true
-    },
     scrolling: false,
 
-    context: 'iframe',
-
     // The url that will be loaded in the iframe or popup, when someone includes my component on their page
-    url: 'https://2e2d2a55.ngrok.io/button',
+    url: '/button',
 
-    // The size of the component on their page
-    dimensions: {
-        width: 350,
-        height: 450
-    },
+    
 
     // The properties they can (or must) pass down to my component
     props: {
--- public/js/lightbox/component.js
+++ public/js/lightbox/component.js
@@ -5,21 +5,20 @@ window.MyLightbox = xcomponent.create({
 
     sandboxContainer: false,
 
+    dimensions: {
+        width: '90%',
+        height: '50%'
+    },
+
+    scrolling: true,
+
+    containerTemplate: xcomponent.containerTemplate,
+
     // The html tag used to render my component
     tag: 'my-lightbox-component',
 
     // The url that will be loaded in the iframe or popup, when someone includes my component on their page
-    url: 'https://2e2d2a55.ngrok.io/lightbox',
-
-    context: 'lightbox',
-
-    // The size of the component on their page
-    dimensions: {
-        width: 350,
-        height: 450
-    },
-
-    remoteRenderDomain: /.*/,
+    url: '/lightbox',
 
     // The properties they can (or must) pass down to my component
     props: {
